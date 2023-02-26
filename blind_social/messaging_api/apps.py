3c09510d00e4881d54c292ae0ee638a287e0bc7c
Adrian Dorado <rbbitphoto@gmail.com>
2023-02-26 02:20:14 -0800

    added more granular apps to 'blind_social', forum, posts, and messaging respectively

--- blind_social/messaging_api/apps.py
+++ blind_social/messaging_api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SocialApiConfig(AppConfig):
+class MessagingApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "social_api"
+    name = "messaging_api"
